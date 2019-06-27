60741d5c65e331a8f5f59b3ddbe79c58cceb6b53
13032463571 <1217432766@qq.com>
2019-06-27 20:43:46 +0800

    配置服务

--- product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
@@ -3,6 +3,8 @@ package cn.how2j.springcloud.controller;
 import cn.how2j.springcloud.pojo.Product;
 import cn.how2j.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@RefreshScope
 public class ProductController {
+
+    @Value("${version}")
+    String version;
     @Autowired private ProductService productService;
 
     @RequestMapping("/products")
     public Object products(Model model) {
         List<Product> ps = productService.listProducts();
         model.addAttribute("products", ps);
+        model.addAttribute("version", version);
         return "products";
     }
 }
